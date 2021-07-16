660381c7b064bc03650c984556220b966cefc404
Mohd Ahmad <dexk2015@gmail.com>
2021-07-16 08:54:42 +0530

    project configured

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const WindiCSS = require("windicss-webpack-plugin").default;
+
 module.exports = {
   reactStrictMode: true,
-}
+
+  webpack(config) {
+    config.plugins.push(new WindiCSS());
+    return config;
+  },
+};
